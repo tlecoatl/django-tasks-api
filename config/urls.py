4a9dac73f28994f56e07ea54c2b2d090225d51fc
Tlecoatl <tlecoatl12@gmail.com>
2025-09-11 21:03:50 -0700

    Add the correct path to the Refresh endpoint

--- config/urls.py
+++ config/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include("rest_framework.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path('api/token/', TokenRefreshView.as_view(), name="token_refresh"),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
     path('api/', include(router.urls)),
     path('api/register/', RegisterUserView.as_view())
 ]
